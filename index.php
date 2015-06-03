702bc8595a5625c05da61403f5a87f01fc9b7a98
glaswr <glaswr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-03 04:50:46 +0000

    changing the description
    git-svn-id: https://plugins.svn.wordpress.org/wp-russian-horoscope/trunk@1173311 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Russian Horoscope
 Plugin URI: http://glaswr.ru/
-Description: Плагин для отображения ежедневного гороскопа.
+Description: Плагин для отображения ежедневного гороскопа. С возможностью выбора дополнительной категории.
 Version: 1.0
 Author: Galswr
 Author URI: http://glaswr.ru/
